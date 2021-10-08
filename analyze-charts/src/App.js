bacd3ba0040c1de6eaecdf04ec7da89457d8a72f
dorothysyu <yu.dor@husky.neu.edu>
2021-10-07 20:10:45 -0400

    more formatting

--- analyze-charts/src/App.js
+++ analyze-charts/src/App.js
@@ -6,86 +6,88 @@ import data from './data'
 import { format } from 'date-fns'
 
 function App() {
-  /* Ensures that data isn't recreated on every render. */
-  const reusableData = React.useMemo(
-    () => data, []
-  )
+    /* Ensures that data isn't recreated on every render. */
+    const reusableData = React.useMemo(
+        () => data, []
+    )
 
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: "Chart Name",
-        accessor: "Chart Name",
-        isVisible: false
-      },
-      {
-        Header: "Chart Week",
-        accessor: "Chart Week",
-        Cell: ({ value }) => { return format(new Date(value), 'MMM d, Y') }
-      },
-      {
-        Header: "Position",
-        accessor: "Position",
-        className: "number"
-      },
-      {
-        Header: "Title",
-        accessor: "Title"
-      },
-      {
-        Header: "Artist",
-        accessor: "Artist"
-      },
-      {
-        Header: "Song ID",
-        accessor: "Song ID"
-      },
-      {
-        Header: "Streams",
-        accessor: "Streams",
-        className: "track-cell number",
-        Cell: ({ value }) => {
-          const disp = value > 0 ? value.toLocaleString() : '-'
-          return disp
-        }
-      },
-      {
-        Header: "Weeks on Chart",
-        accessor: "Weeks on Chart",
-        className: "number",
-        Cell: ({ value }) => {
-          const disp = value > 0 ? value : '-'
-          return disp
-        }
-      },
-      {
-        Header: "Peak Position",
-        accessor: "Peak Position",
-        className: "number",
-        Cell: ({ value }) => {
-          const disp = value > 0 ? value : '-'
-          return disp
-        }
-      },
-      {
-        Header: "Last Position",
-        accessor: "Last Position",
-        className: "number",
-        Cell: ({ value }) => {
-          const disp = value > 0 ? value : '-'
-          return disp
-        }
-      },
-    ],
-    []
-  )
+    const columns = React.useMemo(
+        () => [{
+            Header: "Chart Name",
+            accessor: "Chart Name",
+            show: false
+        },
+        {
+            Header: "Chart Week",
+            accessor: "Chart Week",
+            show: false,
+            Cell: ({ value }) => { return format(new Date(value), 'MMM d, Y') }
+        },
+        {
+            Header: "",
+            accessor: "Position",
+            className: "number"
+        },
+        {
+            Header: "",
+            accessor: "Title",
+            className: "title"
+        },
+        {
+            Header: "",
+            accessor: "Artist",
+            className: "artist"
+        },
+        {
+            Header: "Song ID",
+            accessor: "Song ID",
+            show: false
+        },
+        {
+            Header: "Spotify Streams",
+            accessor: "Streams",
+            className: "track-cell number",
+            Cell: ({ value }) => {
+                const disp = value > 0 ? value.toLocaleString() : '-'
+                return disp
+            }
+        },
+        {
+            Header: "Billboard Weeks on Chart",
+            accessor: "Weeks on Chart",
+            className: "number",
+            Cell: ({ value }) => {
+                const disp = value > 0 ? value : '-'
+                return disp
+            }
+        },
+        {
+            Header: "Billboard Peak Position",
+            accessor: "Peak Position",
+            className: "number",
+            Cell: ({ value }) => {
+                const disp = value > 0 ? value : '-'
+                return disp
+            }
+        },
+        {
+            Header: "Billboard Last Position",
+            accessor: "Last Position",
+            className: "number",
+            Cell: ({ value }) => {
+                const disp = value > 0 ? value : '-'
+                return disp
+            }
+        },
+        ], []
+    )
 
-  return (
-    <div className="container">
-      <Header title='Music Chart Warehouse' />
-      <Table columns={columns} data={reusableData} />
-    </div>
-  );
+    return (
+        <div className="container">
+            <Header title='Music Chart Warehouse' />
+            <Table columns={columns} data={reusableData} />
+        </div>
+    );
 }
 
 export default App
